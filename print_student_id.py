13ce5337f19e80d69a8e224884073a9a97476474
PeemSupeem <124992416+PeemSupeem@users.noreply.github.com>
2023-02-10 15:06:17 +0700

    Add calculate

--- print_student_id.py
+++ print_student_id.py
@@ -2,3 +2,10 @@ def print_id(student_id):
     print("Student ID=", student_id)
 if __name__== "__main__":
     print_id("34567")
+
+def calculate_sum(array):
+    return array.sum()
+
+if __name__== "__main__":
+    print_id("34567")
+    
